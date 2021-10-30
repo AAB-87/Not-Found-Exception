f0365a3347540aa0e7c9c096044590fa31dc3b5e
AAB-87 <andrej-abrekov@yandex.ru>
2021-10-30 15:56:19 +0400

    Initial commit

---
+++ src/main/java/ru/netology/repository/ProductRepository.java
@@ -0,0 +1,46 @@
+package ru.netology.repository;
+
+import jdk.swing.interop.SwingInterOpUtils;
+import ru.netology.domain.Product;
+
+public class ProductRepository { // репозиторий позволяющий сохранять Product'ы, получать все сохранённые Product'ы и удалять по id
+    private Product[] products = new Product[0]; // для этого репозиторий будет хранить у себя поле с типом Product[] (массив товаров)
+
+    public void save(Product product) { // сохранение Product'а
+        int length = products.length + 1; // // вычисляем и создаём новый массив размером на единицу больше
+        Product[] tmp = new Product[length]; // создаём временный массив с новой длиной (на единицу больше)
+        System.arraycopy(products, 0, tmp, 0, products.length);// копируем массив (откуда копируем, с какого места копируем, куда копируем, с какого места начать заполнение нового массива, кол-во элементов которое хотим скопировать)
+        int lastIndex = tmp.length - 1; // добавляем последний новый продукт
+        tmp[lastIndex] = product;
+        products = tmp; // в поле продукт запоминает тот адрес массива который лежит в тмп
+    }
+
+    public Product[] findAll() { // получение Product'а
+        return products; // при вызове метода возвращать массив всех продуктов
+    }
+
+    public void findById(int id) { // поиск продукта по идентификатору
+        for (Product product : products) { // перебираем все продукты в поиске необходимого
+            if (product.getId() == id) { // если находится необходимый идентификатор
+                return product;
+            }
+        }
+    }
+
+    public void removeById(int id) { // удаление Product'а по id
+        if (products.findById == null) {
+            throw new NotFoundException("Element with id: " + id + " not found");
+        }
+        int length = products.length - 1;
+        Product[] tmp = new Product[length];
+//        try {
+        int index = 0;
+        for (Product product : products) {
+            if (product.getId() != id) {
+                tmp[index] = product;
+                index++;
+            }
+        }
+        products = tmp;
+    }
+}
